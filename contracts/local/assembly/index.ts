d50a42b3a416d761d47dcaad9c4af2cae40ba17c
noe <noemk2@gmail.com>
2021-11-08 21:58:56 -0500

    finally

--- contracts/local/assembly/index.ts
+++ contracts/local/assembly/index.ts
@@ -1,13 +1,23 @@
-import {Context} from "near-sdk-as";
+import {context, ContractPromise, logging, u128} from "near-sdk-as";
 
-//near view
-export function hello_world(): string {
-	return "Hello world";
+
+const CONTRACT_NAME = includeBytes("../../remote/neardev/dev-account")
+
+const ONE_TERAGAS = 1000000000000
+const FIVE_TERAGAS = 5 * ONE_TERAGAS
+
+
+export function call_hello_world(): void {
+	const data = CONTRACT_NAME
+
+	let promise = ContractPromise.create(
+		String.UTF8.decodeUnsafe(changetype<usize>(data), data.length),
+		"hello_world",
+		'{}',
+		FIVE_TERAGAS,
+		u128.Zero
+	)
+	promise.returnAsResult()
 }
 
-//near call 
-export function hello(): string {
-	let user = Context.sender
-	return "Hello " + user;
 
-}
